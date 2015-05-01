b46beadddb88c9fe90a90a8aca79e1bdf42d75b3
mbax <matt@phozop.net>
2015-04-30 20:59:23 -0400

    Support permission filter

--- src/main/java/org/kitteh/craftirc/sponge/MinecraftEndpoint.java
+++ src/main/java/org/kitteh/craftirc/sponge/MinecraftEndpoint.java
@@ -23,21 +23,25 @@
  */
 package org.kitteh.craftirc.sponge;
 
+import com.google.common.base.Optional;
 import org.kitteh.craftirc.endpoint.Endpoint;
 import org.kitteh.craftirc.endpoint.Message;
 import org.kitteh.craftirc.endpoint.TargetedMessage;
 import org.kitteh.craftirc.util.MinecraftPlayer;
 import org.kitteh.craftirc.util.loadable.Loadable;
+import org.spongepowered.api.entity.player.Player;
 import org.spongepowered.api.event.Subscribe;
 import org.spongepowered.api.event.entity.player.PlayerChatEvent;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.Texts;
 
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -57,7 +61,7 @@ public class MinecraftEndpoint extends Endpoint {
 
     @Override
     protected void preProcessReceivedMessage(TargetedMessage message) {
-        List<MinecraftPlayer> players = this.plugin.getGame().getServer().getOnlinePlayers().stream().map(player -> new MinecraftPlayer(player.getName(), player.getUniqueId())).collect(Collectors.toCollection(LinkedList::new));
+        Set<MinecraftPlayer> players = this.playerCollectionToMinecraftPlayer(this.plugin.getGame().getServer().getOnlinePlayers());
         message.getCustomData().put(MinecraftEndpoint.PLAYER_LIST, players);
     }
 
@@ -65,25 +69,18 @@ public class MinecraftEndpoint extends Endpoint {
     protected void receiveMessage(TargetedMessage message) {
         @SuppressWarnings("unchecked")
         List<MinecraftPlayer> recipients = (List<MinecraftPlayer>) message.getCustomData().get(MinecraftEndpoint.PLAYER_LIST);
-        /* TODO wait for recipient tracking
         for (MinecraftPlayer recipient : recipients) {
             Optional<Player> player = this.plugin.getGame().getServer().getPlayer(recipient.getName());
             if (player.isPresent()) {
                 player.get().sendMessage(Texts.of(message.getCustomMessage()));
             }
-        }*/
+        }
         this.plugin.getGame().getServer().getOnlinePlayers().forEach(player -> player.sendMessage(Texts.of(message.getCustomMessage())));
     }
 
     @Subscribe
     public void onChat(PlayerChatEvent event) {
         Map<String, Object> data = new HashMap<>();
-        /* TODO wait for recipient tracking
-        Set<MinecraftPlayer> recipients = new HashSet<>();
-        for (Player player : event.getRecipients()) {
-            recipients.add(new MinecraftPlayer(player.getName(), player.getUniqueId()));
-        } */
-        // String format = event.getFormat(); TODO wait for format
         Text text = event.getMessage();
         if (text instanceof Text.Translatable) {
             Text.Translatable trans = (Text.Translatable) text;
@@ -92,9 +89,14 @@ public class MinecraftEndpoint extends Endpoint {
             String format = trans.getTranslation().get(Locale.ENGLISH);
             data.put(Endpoint.MESSAGE_FORMAT, format);
             data.put(Endpoint.MESSAGE_TEXT, message);
-            // data.put(MinecraftEndpoint.PLAYER_LIST, recipients); TODO wait for recipient tracking
+            Set<MinecraftPlayer> recipients = this.playerCollectionToMinecraftPlayer(this.plugin.getGame().getServer().getOnlinePlayers()); // TODO Collect recipients per event here.
+            data.put(MinecraftEndpoint.PLAYER_LIST, recipients);
             data.put(Endpoint.SENDER_NAME, sender);
             this.plugin.getCraftIRC().getEndpointManager().sendMessage(new Message(this, String.format(format, sender, message), data));
         }
+    }
+
+    private Set<MinecraftPlayer> playerCollectionToMinecraftPlayer(Collection<Player> collection) {
+        return collection.stream().map(player -> new MinecraftPlayer(player.getName(), player.getUniqueId())).collect(Collectors.toCollection(HashSet::new));
     }
 }
